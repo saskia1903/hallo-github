a441c539c0976c7ef16899832372fc7587a2900b
saskia1903 <84993664+saskia1903@users.noreply.github.com>
2021-05-29 15:43:52 +0200

    Delete head.php

--- head.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<meta title="author1" name="Christopher Hufnagel">
-<meta title="author2" name="Saskia Landgraf">
-<meta title="author3" name="Martin Stöhr">
-<link rel="stylesheet" href="allgemein.css">
-<title>Umfrage zum 2. Weltkrieg</title>
