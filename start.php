e742821e3a6c9b3f1ff28e274473772c9edf687b
saskia1903 <84993664+saskia1903@users.noreply.github.com>
2021-05-29 15:36:04 +0200

    Update start.php

--- start.php
+++ start.php
@@ -6,7 +6,8 @@
         <meta title="author1" name="Christopher Hufnagel">
         <meta title="author2" name="Saskia Landgraf">
         <meta title="author3" name="Martin Stöhr">
-        <link rel="stylesheet" href="allgemein.css">
+        
+        <link rel="stylesheet" type="text/css" href="allgemien.css">
         <title>Umfrage zum 2. Weltkrieg</title>
         <link rel="stylesheet" href="start.css">
     </head>
